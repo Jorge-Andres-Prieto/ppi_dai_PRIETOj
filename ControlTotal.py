55c5ad63a5513ace43d56a88ca70d60d1f60ad98
ANDRÉS JIMÉNEZ <161528069+Jorge-Andres-Prieto@users.noreply.github.com>
2024-04-14 23:17:31 -0500

    Update ControlTotal.py

--- ControlTotal.py
+++ ControlTotal.py
@@ -16,7 +16,7 @@ class User(Base):
     phone_number = Column(String, nullable=False)
 
 # Conexión a la base de datos PostgreSQL
-DATABASE_URL = st.secrets["database"]["DATABASE_URL"]
+DATABASE_URL = "postgresql://datos_usuarios_user:NNgnrDUS7HG3zQPuffAWnG3pyDvevRs2@dpg-coe966gl6cac73bvqv3g-a.oregon-postgres.render.com/datos_usuarios"
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
