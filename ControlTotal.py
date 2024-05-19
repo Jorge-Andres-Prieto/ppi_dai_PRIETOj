b10c4d39334c98f9e9c6499237ef6f314483f8d0
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-18 20:52:25 -0500

    Cambio 195

--- ControlTotal.py
+++ ControlTotal.py
@@ -671,23 +671,46 @@ def handle_sales():
 
 
 def create_sale(user_id, total_efectivo, total_transferencia, productos_vendidos, total_credito, sitio):
+    """Registra una venta en la base de datos.
+
+    Args:
+        user_id (int): ID del usuario que realiza la venta.
+        total_efectivo (float): Total pagado en efectivo.
+        total_transferencia (float): Total pagado por transferencia.
+        productos_vendidos (list): Lista de productos vendidos.
+        total_credito (float): Total pagado en crédito.
+        sitio (str): Ubicación del punto de venta (tienda o bodega).
+
+    Returns:
+        str: Mensaje indicando si la venta fue registrada con éxito o no.
+    """
     session = Session()
     try:
-        productos_vendidos_str = ', '.join(
-            [f"{item['product'].name} x {item['quantity']}" for item in productos_vendidos])
-        new_sale = Venta(user_id=user_id, total_efectivo=total_efectivo, total_transferencia=total_transferencia,
-                         productos_vendidos=productos_vendidos_str, total_credito=total_credito)
-        session.add(new_sale)
+        # Obtener la fecha y hora actual
+        fecha_hora = datetime.now()
 
-        # Descontar la cantidad de productos vendidos del inventario en tienda o bodega
+        # Crear una nueva venta
+        new_sale = Venta(
+            user_id=user_id,
+            fecha_hora=fecha_hora,
+            total_efectivo=total_efectivo,
+            total_transferencia=total_transferencia,
+            total_credito=total_credito,
+            productos_vendidos=[{
+                'product_id': item['product'].product_id,
+                'cantidad': item['quantity']
+            } for item in productos_vendidos]
+        )
+
+        # Actualizar inventario
         for item in productos_vendidos:
-            product = session.query(Product).filter(Product.product_id == item['product'].product_id).one_or_none()
-            if product:
-                if sitio == 'Tienda':
-                    product.total_tienda -= item['quantity']
-                else:
-                    product.total_bodega -= item['quantity']
+            product = session.query(Product).filter(Product.product_id == item['product'].product_id).first()
+            if sitio == 'Tienda':
+                product.total_tienda -= item['quantity']
+            else:
+                product.total_bodega -= item['quantity']
 
+        session.add(new_sale)
         session.commit()
         return "Venta registrada con éxito."
     except Exception as e:
