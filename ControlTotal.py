91de5ddf3953064841453b3ce66c39c2920e0598
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-19 15:02:31 -0500

    Cambio 216: agregue el logo. hasta el 214 funcionaba bien

--- ControlTotal.py
+++ ControlTotal.py
@@ -133,7 +133,7 @@ def main_menu(user):
         with st.sidebar:
 
             logo_url = "https://i.ibb.co/YQzrD31/Logo-Control-Total-removebg-preview.png"
-            st.image(logo_url, width=200, height=100)
+            st.image(logo_url, width=20, height=20)
 
             selected = option_menu(
                 None,
