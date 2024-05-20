1688f58a6e1105bf4acfffb35c4bdba0e4a45bec
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-20 11:58:34 -0500

    17: hasta la 15 funcionaba bien, en este caso se agregan comentarios a models.py

--- models.py
+++ models.py
@@ -4,15 +4,19 @@ from sqlalchemy.ext.declarative import declarative_base
 # Importa tipos de columnas específicos de SQLAlchemy para definir propiedades de modelo
 from sqlalchemy import Column, Integer, String, Numeric, DateTime, PickleType
 
+# Importa la función relationship de SQLAlchemy para definir relaciones entre modelos
 from sqlalchemy.orm import relationship
 
+# Importa la clase datetime para manejar fechas y horas
 from datetime import datetime
+
 
 # Crea una instancia base para los modelos ORM, que es necesario para definir clases de modelos
 Base = declarative_base()
 
 class User(Base):
-    """Define la estructura de la tabla 'users' para almacenar datos de usuario.
+    """
+    Define la estructura de la tabla 'users' para almacenar datos de usuario.
 
     Attributes:
         id (Column): Identificador único del usuario, clave primaria.
@@ -20,7 +24,8 @@ class User(Base):
         password (Column): Contraseña del usuario, no puede ser nula.
         role (Column): Rol del usuario en la aplicación, no puede ser nulo.
         full_name (Column): Nombre completo del usuario, no puede ser nulo.
-        phone_number (Column): Número de teléfono del usuario, no puede seer nulo.
+        phone_number (Column): Número de teléfono del usuario, no puede ser nulo.
+        tdp (Column): Estado de aceptación de tratamiento de datos personales.
 
     Args:
         None
@@ -39,6 +44,26 @@ class User(Base):
 
 
 class Product(Base):
+    """
+    Define la estructura de la tabla 'products' para almacenar datos de productos.
+
+    Attributes:
+        id (Column): Identificador único del producto, clave primaria.
+        product_id (Column): Identificador del producto, debe ser único y no nulo.
+        name (Column): Nombre del producto, no puede ser nulo.
+        brand (Column): Marca del producto.
+        category (Column): Categoría del producto, no puede ser nula.
+        subcategory (Column): Subcategoría del producto.
+        price (Column): Precio del producto, no puede ser nulo.
+        total_tienda (Column): Cantidad del producto en tienda.
+        total_bodega (Column): Cantidad del producto en bodega.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
     product_id = Column(String, nullable=False, unique=True)
@@ -50,7 +75,25 @@ class Product(Base):
     total_tienda = Column(Integer, nullable=False, default=0)
     total_bodega = Column(Integer, nullable=False, default=0)
 
+
 class Cliente(Base):
+    """
+    Define la estructura de la tabla 'clientes' para almacenar datos de clientes.
+
+    Attributes:
+        id (Column): Identificador único del cliente, clave primaria.
+        nombre (Column): Nombre del cliente, no puede ser nulo.
+        direccion (Column): Dirección del cliente, no puede ser nula.
+        telefono (Column): Teléfono del cliente, no puede ser nulo.
+        cedula (Column): Cédula del cliente, debe ser única y no nula.
+        credito (Column): Crédito del cliente.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     __tablename__ = 'clientes'
     id = Column(Integer, primary_key=True)
     nombre = Column(String, nullable=False)
@@ -59,9 +102,32 @@ class Cliente(Base):
     cedula = Column(String, nullable=False, unique=True)
     credito = Column(Numeric(10, 2))
 
-class Venta(Base):
-    __tablename__ = 'ventas'
 
+class Venta(Base):
+    """
+    Define la estructura de la tabla 'ventas' para almacenar datos de ventas.
+
+    Attributes:
+        id (Column): Identificador único de la venta, clave primaria.
+        user_id (Column): Identificador del usuario que realizó la venta.
+        fecha_hora (Column): Fecha y hora de la venta.
+        total_efectivo (Column): Total de la venta en efectivo.
+        total_transferencia (Column): Total de la venta por transferencia.
+        total_credito (Column): Total de la venta a crédito.
+        productos_vendidos (Column): Productos vendidos en la venta.
+
+    Args:
+        user_id (int): Identificador del usuario que realizó la venta.
+        fecha_hora (datetime): Fecha y hora de la venta.
+        total_efectivo (float): Total de la venta en efectivo.
+        total_transferencia (float): Total de la venta por transferencia.
+        total_credito (float): Total de la venta a crédito.
+        productos_vendidos (str): Productos vendidos en la venta.
+
+    Returns:
+        None
+    """
+    __tablename__ = 'ventas'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     fecha_hora = Column(DateTime, nullable=False)
