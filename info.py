31986483d3dc27f532dd69202fe06688ccbfcafb
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-20 13:04:13 -0500

    19: hasta la 15 funcionaba bien, en este caso se mejora la parte visual de COntrol Total y sobre el Autor

--- info.py
+++ info.py
@@ -49,107 +49,74 @@ Si tienes preguntas sobre esta política o sobre cómo gestionamos tus datos per
 """
 
 info_control_total = """
-<h1 align="center">Bienvenido a Control Total</h1>
+# Bienvenido a Control Total 
 
-<h2 align="center">
-  <img src="https://i.ibb.co/HryLWKK/Logo-Control-Total-removebg-preview.png" alt="Control Total" width="150">
-</h2>
+### ¿Qué es?
+Control Total es una plataforma integral diseñada para optimizar la gestión de inventario y contabilidad en pequeñas y medianas empresas.
 
-<h3>📌 ¿Qué es Control Total?</h3>
-<p>Control Total es una plataforma integral diseñada para optimizar la gestión de inventario y contabilidad en pequeñas y medianas empresas.</p>
+### 🌟 ¿Por qué usar Control Total?
+Control Total ofrece una solución todo-en-uno que simplifica procesos, reduce errores y ahorra tiempo. Con nuestra plataforma, puedes:
 
-<h3>🌟 ¿Por qué usar Control Total?</h3>
-<p>Control Total ofrece una solución todo-en-uno que simplifica procesos, reduce errores y ahorra tiempo. Con nuestra plataforma, puedes:</p>
-<ul>
-  <li>📈 <strong>Gestionar inventarios</strong> de forma eficiente.</li>
-  <li>💵 <strong>Automatizar la facturación</strong> y seguimiento de ventas.</li>
-  <li>📊 <strong>Analizar datos</strong> para tomar decisiones informadas.</li>
-</ul>
+- 📈 **Gestionar inventarios** de forma eficiente.
+- 💵 **Automatizar la facturación** y seguimiento de ventas.
+- 📊 **Analizar datos** para tomar decisiones informadas.
 
-<h3>⚙️ ¿Cómo Funciona?</h3>
-<p>La plataforma está construida sobre una interfaz amigable en Streamlit, permitiendo una fácil navegación y manejo:</p>
-<ol>
-  <li><strong>Registro de Usuario:</strong> Comienza creando tu cuenta de usuario.</li>
-  <li><strong>Configuración de Inventario:</strong> Añade y organiza tus productos.</li>
-  <li><strong>Operaciones Diarias:</strong> Registra ventas, compras y otros movimientos.</li>
-  <li><strong>Reportes:</strong> Accede a reportes detallados y análisis de tus datos.</li>
-</ol>
+### ⚙️ ¿Cómo Funciona?
+La plataforma está construida sobre una interfaz amigable en Streamlit, permitiendo una fácil navegación y manejo:
 
-<h3>🚀 Funcionalidades de Control Total</h3>
-<ul>
-  <li>🔐 <strong>Logueo:</strong> Indica desde qué tienda se está ingresando.</li>
-  <li>📜 <strong>Políticas de tratamiento de datos:</strong> Gestión de usuarios y datos seguros.</li>
-  <li>🛒 <strong>Gestión de Ventas y Facturación:</strong> Procesa ventas y genera facturas.</li>
-  <li>👥 <strong>Gestión de Clientes:</strong> Permite créditos y seguimiento de clientes.</li>
-  <li>📦 <strong>Gestión de Inventarios:</strong> Controla el stock y movimientos de productos.</li>
-  <li>📊 <strong>Análisis Estadísticos:</strong> Ventas diarias, métodos de pago, cuadre de caja, estadísticas descriptivas y pruebas de hipótesis.</li>
-  <li>🚚 <strong>Gestión de Domicilios:</strong> Optimiza las rutas de entrega.</li>
-</ul>
+1. **Registro de Usuario:** Comienza creando tu cuenta de usuario.
+2. **Configuración de Inventario:** Añade y organiza tus productos.
+3. **Operaciones Diarias:** Registra ventas, compras y otros movimientos.
+4. **Reportes:** Accede a reportes detallados y análisis de tus datos.
 
-<h3>📊 Uso de Tecnologías</h3>
-<p>Control Total utiliza las siguientes tecnologías:</p>
-<p>
-  <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"/>
-  <img src="https://img.shields.io/badge/PostgreSQL-336791?style=for-the-badge&logo=postgresql&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=streamlit&logoColor=white"/>
-  <img src="https://img.shields.io/badge/NumPy-013243?style=for-the-badge&logo=numpy&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Pandas-150458?style=for-the-badge&logo=pandas&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Matplotlib-3776AB?style=for-the-badge&logo=python&logoColor=white"/>
-  <img src="https://img.shields.io/badge/SciPy-8CAAE6?style=for-the-badge&logo=scipy&logoColor=white"/>
-  <img src="https://img.shields.io/badge/GeoPandas-1B1B1B?style=for-the-badge&logo=geopandas&logoColor=white"/>
-</p>
+### 🚀 Funcionalidades de Control Total
+- 🔐 **Logueo:** Indica desde qué tienda se está ingresando.
+- 📜 **Políticas de tratamiento de datos:** Gestión de usuarios y datos seguros.
+- 🛒 **Gestión de Ventas y Facturación:** Procesa ventas y genera facturas.
+- 👥 **Gestión de Clientes:** Permite créditos y seguimiento de clientes.
+- 📦 **Gestión de Inventarios:** Controla el stock y movimientos de productos.
+- 📊 **Análisis Estadísticos:** Ventas diarias, métodos de pago, cuadre de caja, estadísticas descriptivas y pruebas de hipótesis.
+- 🚚 **Gestión de Domicilios:** Optimiza las rutas de entrega.
 
-<h3>☕ Buy Me a Coffee</h3>
-<p>Si te gusta mi trabajo, puedes apoyarme invitándome a un café:</p>
-<p>
-  <a href="https://www.buymeacoffee.com/Andres.Jimenez">
-    <img src="https://img.shields.io/badge/Buy%20Me%20a%20Coffee-FFDD00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black" alt="Buy Me a Coffee"/>
-  </a>
-</p>
+### 💻 Uso de Tecnologías
+Control Total utiliza las siguientes tecnologías:
+
+- ![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
+- ![PostgreSQL](https://img.shields.io/badge/PostgreSQL-336791?style=for-the-badge&logo=postgresql&logoColor=white)
+- ![Streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=streamlit&logoColor=white)
+- ![NumPy](https://img.shields.io/badge/NumPy-013243?style=for-the-badge&logo=numpy&logoColor=white)
+- ![Pandas](https://img.shields.io/badge/Pandas-150458?style=for-the-badge&logo=pandas&logoColor=white)
+- ![Matplotlib](https://img.shields.io/badge/Matplotlib-3776AB?style=for-the-badge&logo=python&logoColor=white)
+- ![SciPy](https://img.shields.io/badge/SciPy-8CAAE6?style=for-the-badge&logo=scipy&logoColor=white)
+- ![GeoPandas](https://img.shields.io/badge/GeoPandas-1B1B1B?style=for-the-badge&logo=geopandas&logoColor=white)
+
+### ☕ Buy Me a Coffee
+Si te gusta mi trabajo, puedes apoyarme invitándome a un café:
+
+[![Buy Me a Coffee](https://img.shields.io/badge/Buy%20Me%20a%20Coffee-FFDD00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black)](https://www.buymeacoffee.com/Andres.Jimenez)
 """
 
 
 info_sobre_autor = """
-<h1>Sobre el Autor</h1>
+## Sobre el Autor
 
-<p>Mi nombre es Jorge Prieto, desarrollador y creador de Control Total. Actualmente soy estudiante de ingeniería de sistemas e informática en la Universidad Nacional de Colombia en Medellín.</p>
+Mi nombre es Jorge Prieto, desarrollador y creador de Control Total. Actualmente soy estudiante de ingeniería de sistemas e informática en la Universidad Nacional de Colombia en Medellín.
 
-<h2>🛠️ Tecnologías que Uso</h2>
-<p>Estas son algunas de las tecnologías que utilizo en mi desarrollo:</p>
-<p>
-  <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"/>
-  <img src="https://img.shields.io/badge/NumPy-013243?style=for-the-badge&logo=numpy&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Pandas-150458?style=for-the-badge&logo=pandas&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Matplotlib-3776AB?style=for-the-badge&logo=python&logoColor=white"/>
-  <img src="https://img.shields.io/badge/PostgreSQL-336791?style=for-the-badge&logo=postgresql&logoColor=white"/>
-  <img src="https://img.shields.io/badge/MySQL-4479A1?style=for-the-badge&logo=mysql&logoColor=white"/>
-  <img src="https://img.shields.io/badge/SQL%20Server-CC2927?style=for-the-badge&logo=microsoft-sql-server&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=streamlit&logoColor=white"/>
-</p>
+### 🛠️ Tecnologías que Uso
+- ![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white) He utilizado librerías como NumPy, Pandas y Matplotlib para el análisis de datos y visualización.
+- ![PostgreSQL](https://img.shields.io/badge/PostgreSQL-336791?style=for-the-badge&logo=postgresql&logoColor=white) Experiencia con PostgreSQL, MySQL y SQL Server, gestionando datos de forma segura y eficiente.
+- ![Streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=streamlit&logoColor=white) Desarrollo de aplicaciones web usando Streamlit, lo que permite una interfaz interactiva y moderna.
 
-<p>He creado esta aplicación con el propósito de ayudar a las pequeñas y medianas empresas a mejorar la forma en que llevan el inventario y la contabilidad de sus negocios.</p>
+He creado esta aplicación con el propósito de ayudar a las pequeñas y medianas empresas a mejorar la forma en que llevan el inventario y la contabilidad de sus negocios.
 
-<h2>📬 Contáctame</h2>
-<p>Si deseas contactarme, puedes hacerlo a través de:</p>
-<ul>
-  <li><strong>Correo Electrónico:</strong> <a href="mailto:prietojorge905@gmail.com">prietojorge905@gmail.com</a></li>
-  <li><strong>Teléfono:</strong> 3104219762</li>
-</ul>
+### 📬 Contáctame
+- **Correo Electrónico:** prietojorge905@gmail.com
+- **Teléfono:** 3104219762
 
-<h2>🌐 Encuéntrame en</h2>
-<p>
-  <a href="https://github.com/Jorge-Andres-Prieto/ppi_pl_PRIETOj/blob/main/README.md">
-    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub"/>
-  </a>
-  <a href="https://www.linkedin.com/in/jorge-prieto-b36ab2250/">
-    <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn"/>
-  </a>
-  <a href="https://www.upwork.com/freelancers/~0142bd1ae6229261b5">
-    <img src="https://img.shields.io/badge/Upwork-6FDA44?style=for-the-badge&logo=upwork&logoColor=white" alt="Upwork"/>
-  </a>
-  <a href="https://www.reddit.com/user/Flestar/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button">
-    <img src="https://img.shields.io/badge/Reddit-FF4500?style=for-the-badge&logo=reddit&logoColor=white" alt="Reddit"/>
-  </a>
-</p>
+También puedes contactarme en:
+
+[![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/Jorge-Andres-Prieto/ppi_pl_PRIETOj/blob/main/README.md)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/jorge-prieto-b36ab2250/)
+[![Upwork](https://img.shields.io/badge/Upwork-6FDA44?style=for-the-badge&logo=upwork&logoColor=white)](https://www.upwork.com/freelancers/~0142bd1ae6229261b5)
+[![Reddit](https://img.shields.io/badge/Reddit-FF4500?style=for-the-badge&logo=reddit&logoColor=white)](https://www.reddit.com/user/Flestar/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)
 """
-
